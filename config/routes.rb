ed3c6bc43e5b5a101b055349d0f5c42fa3380f8e
matheus-fls <matheus_fls@yahoo.com.br>
2019-10-16 13:19:00 -0300

    Set up RSpec gems and basic files

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-
 end
---
+++ spec/controllers/user_controller_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe UsersController do
+  describe '' do
+    it '' do
+      
+    end
+  end
+end
