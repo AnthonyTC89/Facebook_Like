e268e5cdfffdad4724daf926d55dc0c72a0fe84e
AnthonyTC89 <ptonyptc@gmail.com>
2019-10-18 17:30:05 -0500

    phone number field added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,10 @@ class ApplicationController < ActionController::Base
   protected
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation, :gender, :birthday)}
-      devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :email, :password, :gender, :birthday, :current_password, :password_confirmation)}
+      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, 
+        :email, :password, :password_confirmation, :gender, :birthday, :phone)}
+      devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, 
+      :email, :password, :gender, :birthday, :phone, :current_password, :password_confirmation)}
     end
 
     def after_sign_in_path_for(resource)
