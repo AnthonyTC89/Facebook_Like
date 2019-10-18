48e5e632b24b4e54af02f7c1df2a752b8dfe80a3
matheus-fls <matheus_fls@yahoo.com.br>
2019-10-18 15:03:54 -0300

    Update user model, posts create and show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,28 @@
 class PostsController < ApplicationController
+  def new 
+  end
+
+  def create 
+    @post = current_user.posts.build(post_params)
+    if @post.valid? 
+      @post.save
+      redirect_to current_user
+    else
+      render current_user
+    end
+    p @post
+  end
+
+  def show 
+    @posts = Post.where('user_id = ?', current_user)
+  end
+
+  def delete 
+  end
+
+  private 
+
+    def post_params 
+      params.require(:post).permit(:content)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :posts
+  
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
 
   validates :first_name, presence: true, length: { maximum: 50 }
