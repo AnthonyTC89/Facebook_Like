84ca1f43acb24b1db2eb20cbaaed02eb4981e79d
matheus-fls <matheus_fls@yahoo.com.br>
2019-10-21 16:31:12 -0300

    Add default scope for post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,8 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
   before_destroy :cleanup
+  default_scope -> { order(created_at: :desc) }
+
   validates :content, presence: true, length: { maximum: 250 }
 
   private 
