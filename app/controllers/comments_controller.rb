715c9f10cc9c022303d685a93ce0c7351323f02e
AnthonyTC89 <ptonyptc@gmail.com>
2019-10-21 12:02:30 -0500

    partial views created, form for comments, add form comments to user show

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,8 +3,7 @@ class CommentsController < ApplicationController
   end
 
   def create 
-    @post = Post.find(params[:post_id])
-    @comment = @post.comments.build(comment_params)
+    @comment = Comment.new(comment_params)
     @comment.user_id = current_user.id 
     if @comment.valid?
       @comment.save
@@ -23,6 +22,6 @@ class CommentsController < ApplicationController
   private 
 
     def comment_params
-      params.require(:comment).permit(:user_id, :post_id, :content)
+      params.require(:comment).permit(:post_id, :content)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,5 @@ Rails.application.routes.draw do
 
   resources :users,    only: :show
   resources :posts
+  resources :comments
 end
