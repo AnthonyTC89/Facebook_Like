ace4efcb5ab828c1ab80c80624bae722d46d40a2
matheus-fls <matheus_fls@yahoo.com.br>
2019-10-22 14:31:39 -0300

    Replace redirect with render on likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -7,12 +7,12 @@ class LikesController < ApplicationController
     @like = Like.new(user: current_user, likeable_id: params[:id].to_i, 
       likeable_type: params[:type])
     @like.save
-    redirect_to current_user 
+    render :template => 'users/show'
   end
 
   def destroy
     @like = Like.find(params[:id].to_i)
     @like.destroy
-    redirect_to current_user
+    render :template => 'users/show'
   end
 end
