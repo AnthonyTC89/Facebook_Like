5b56cdc9150ee03128dd477807719e721c7cd5d4
AnthonyTC89 <ptonyptc@gmail.com>
2019-10-22 15:41:26 -0500

    before action: authenticate!

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class PostsController < ApplicationController
+ 
   def new; end
 
   def create
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+
   def show
     @posts = Post.where('user_id = ?', current_user)
   end
