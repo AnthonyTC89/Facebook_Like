67125c97c46a5b320b0fe6f19e57b12ce9996405
matheus-fls <matheus_fls@yahoo.com.br>
2019-10-22 14:22:15 -0300

    Finish like/dislike action

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,5 +1,5 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :likeable, :polymorphic => true
-  validates_uniqueness_of :user_id, scope: :likeable_id
+  validates_uniqueness_of :user_id, { scope: [:likeable_id, :likeable_type] }
 end
