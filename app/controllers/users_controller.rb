5bd3f6a6b2ed01fa50e30457d08968bd62bf058c
matheus-fls <matheus_fls@yahoo.com.br>
2019-10-23 18:22:34 -0300

    Improvements to user show, add user index on controller, improve view, add authenticate_user to controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  before_action :authenticate_user!
   def show
-    @posts = current_user.posts
+    @user = User.find(params[:id])
+  end
+
+  def index 
+    @users = User.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,6 @@ Rails.application.routes.draw do
     root 'devise/registrations#new'
   end
 
-  resources :users,    only: :show
+  resources :users
   resources :posts
 end
