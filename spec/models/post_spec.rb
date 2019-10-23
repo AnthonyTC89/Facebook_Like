dce919e3436b145b7220bf19ad6c971d31461b1d
AnthonyTC89 <ptonyptc@gmail.com>
2019-10-23 10:38:02 -0500

    test from controller to model, rspec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,5 +3,31 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  
+  let(:user) do
+    User.create!(first_name: 'Foo', email: 'foo@bar.com',
+                 password: '123456', password_confirmation: '123456')
+  end
+
+  describe 'Creating a valid post' do
+    it 'Post with valid fields' do
+      @post = user.posts.build(content: 'content test')
+      expect(@post).to be_valid
+    end
+
+    it 'Post without content' do
+      @post = user.posts.build
+      expect(@post).to_not be_valid
+    end
+
+    it 'Post with valid fields' do
+      @post = user.posts.build(content: 'p' * 300)
+      expect(@post).to_not be_valid
+    end
+
+    it 'Post without user' do
+      @post = Post.new(content: 'content test')
+      expect(@post).to_not be_valid
+    end
+  end
 end
