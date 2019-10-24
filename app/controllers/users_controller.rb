e3261f9c06be154f14fca1c9672738c7a1874d65
matheus-fls <matheus_fls@yahoo.com.br>
2019-10-24 16:46:54 -0300

    Add before_action :authenticate_user! to all controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  before_action :authenticate_user!
 
   def show
     @posts = Post.where('user_id = ?', current_user.posts)
