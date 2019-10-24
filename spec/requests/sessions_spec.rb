74e55b275c643b8834993ae8500ca18ee30a50a8
AnthonyTC89 <ptonyptc@gmail.com>
2019-10-24 10:16:31 -0500

    sign in and sign out tests done

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -1,3 +1,5 @@
+
+
 RSpec.describe "Sessions" do
   it "signs user in and out" do
     # user = create(:user)    ## uncomment if using FactoryBot
@@ -6,11 +8,12 @@ RSpec.describe "Sessions" do
     sign_in user
     visit root_path
     expect(page).to have_content('Test') 
-    #expect(response).to render_template(:show) # add gem 'rails-controller-testing' to your Gemfile first.
+    expect(current_path).to eql("/users/show") # add gem 'rails-controller-testing' to your Gemfile first.
     
     sign_out user
     visit root_path
     expect(page).to have_content('Sign up') 
-    # expect(response).not_to render_template(:index) # add gem 'rails-controller-testing' to your Gemfile first.
+    expect(current_path).to eql("/")
+    #expect(response).not_to render_template(:index) # add gem 'rails-controller-testing' to your Gemfile first.
   end
 end
