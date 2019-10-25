9b747bedb08c94e3ef6a9e5a57cd01cd84df0821
matheus-fls <matheus_fls@yahoo.com.br>
2019-10-25 19:08:19 -0300

    Add index buttons to friendship

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,18 +1,14 @@
 class FriendshipsController < ApplicationController
-
-  
   def create
     @user = User.find(params[:user_id])
     @friend = User.find(params[:friend_id])
     if @user.friend?(@friend) || @friend.friend_requests.include?(@user)
-      #Flash
-      p "not created"
+      flash[:danger] = 'Friend request not sent'
     else
-      Friendship.create(user_id: @user, friend_id: @friend)
-      #Flash
-      p "created"
+      Friendship.create(user: @user, friend: @friend)
+      flash[:success] = 'Friend request sent'
     end
-    
+    redirect_to request.referrer
   end
 
   def update
