2726e7a7c869ed72e1a10c33dff7127886fef956
AnthonyTC89 <ptonyptc@gmail.com>
2019-10-25 16:20:22 -0500

    validates in friendship model

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,8 +1,18 @@
 class FriendshipsController < ApplicationController
+
+  
   def create
-    @user = params[:user_id]
-    @friend = params[:friend_id]
-    Friendship.create(user_id: @user, friend_id: @friend)
+    @user = User.find(params[:user_id])
+    @friend = User.find(params[:friend_id])
+    if @user.friend?(@friend) || @friend.friend_requests.include?(@user)
+      #Flash
+      p "not created"
+    else
+      Friendship.create(user_id: @user, friend_id: @friend)
+      #Flash
+      p "created"
+    end
+    
   end
 
   def update
