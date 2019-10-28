30266c2e7cb02fe38afa80be13b681e4ca33cd4d
matheus-fls <matheus_fls@yahoo.com.br>
2019-10-28 13:42:57 -0300

    Add test for friendship create

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -1,5 +1,27 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Friendship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:user) do
+    User.create(first_name: 'Test', email: 'test@test.com',
+                password: 'password', password_confirmation: 'password')
+  end
+
+  let!(:user_2) do
+    User.create(first_name: 'Test-2', email: 'test-2@test.com',
+                password: 'password', password_confirmation: 'password')
+  end
+
+  describe 'Creates and destroys friendships' do
+    it 'creates a friendship' do
+      Friendship.create(user: user, friend: user_2)
+      expect(Friendship.count).to eql(1)
+      expect(user_2.friend?(user)).to be_falsey
+      expect(user.friend?(user_2)).to be_falsey
+      user_2.confirm_friend(user)
+      expect(user_2.friend?(user)).to be true
+      expect(user.friend?(user_2)).to be true
+    end
+  end
 end
