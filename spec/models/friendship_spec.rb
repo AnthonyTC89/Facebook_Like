d5a45fb2047cd2572b8c4d7e73ad9a2b0b61e113
AnthonyTC89 <ptonyptc@gmail.com>
2019-10-28 12:00:11 -0500

    create and destroy friendships model test rspec

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -17,11 +17,43 @@ RSpec.describe Friendship, type: :model do
     it 'creates a friendship' do
       Friendship.create(user: user, friend: user_2)
       expect(Friendship.count).to eql(1)
-      expect(user_2.friend?(user)).to be_falsey
       expect(user.friend?(user_2)).to be_falsey
+      expect(user_2.friend?(user)).to be_falsey
+
       user_2.confirm_friend(user)
+      user = User.first
+      user_2 = User.last
+
       expect(user_2.friend?(user)).to be true
       expect(user.friend?(user_2)).to be true
+    end
+
+    it 'canceling friendships' do
+      Friendship.create(user: user, friend: user_2)
+
+      user = User.first
+      user_2 = User.last
+
+      expect(Friendship.count).to eql(1)
+      expect(user.friend?(user_2)).to be_falsey
+      expect(user_2.friend?(user)).to be_falsey
+
+      user_2.confirm_friend(user)
+      user = User.first
+      user_2 = User.last
+
+      expect(user_2.friend?(user)).to be true
+      expect(user.friend?(user_2)).to be true
+
+      friendship = Friendship.where("user_id = ? and friend_id = ?", user.id, user_2.id).first
+      friendship.destroy
+      expect(Friendship.count).to eql(0)
+
+      user = User.first
+      user_2 = User.last
+
+      expect(user_2.friend?(user)).to be false
+      expect(user.friend?(user_2)).to be false
     end
   end
 end
