acba9599e47a67d8430f0aaca9b378d531733756
matheus-fls <matheus_fls@yahoo.com.br>
2019-10-28 14:36:12 -0300

    Fix linter errors

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -8,52 +8,52 @@ RSpec.describe Friendship, type: :model do
                 password: 'password', password_confirmation: 'password')
   end
 
-  let!(:user_2) do
+  let!(:user2) do
     User.create(first_name: 'Test-2', email: 'test-2@test.com',
                 password: 'password', password_confirmation: 'password')
   end
 
   describe 'Creates and destroys friendships' do
     it 'creates and confirm friendship' do
-      Friendship.create(user: user, friend: user_2)
+      Friendship.create(user: user, friend: user2)
       expect(Friendship.count).to eql(1)
-      expect(user.friend?(user_2)).to be_falsey
-      expect(user_2.friend?(user)).to be_falsey
+      expect(user.friend?(user2)).to be_falsey
+      expect(user2.friend?(user)).to be_falsey
 
-      user_2.confirm_friend(user)
+      user2.confirm_friend(user)
       user = User.first
-      user_2 = User.last
+      user2 = User.last
 
-      expect(user_2.friend?(user)).to be true
-      expect(user.friend?(user_2)).to be true
+      expect(user2.friend?(user)).to be true
+      expect(user.friend?(user2)).to be true
     end
 
     it 'canceling friendships' do
-      Friendship.create(user: user, friend: user_2)
+      Friendship.create(user: user, friend: user2)
 
       user = User.first
-      user_2 = User.last
+      user2 = User.last
 
       expect(Friendship.count).to eql(1)
-      expect(user.friend?(user_2)).to be_falsey
-      expect(user_2.friend?(user)).to be_falsey
+      expect(user.friend?(user2)).to be_falsey
+      expect(user2.friend?(user)).to be_falsey
 
-      user_2.confirm_friend(user)
+      user2.confirm_friend(user)
       user = User.first
-      user_2 = User.last
+      user2 = User.last
 
-      expect(user_2.friend?(user)).to be true
-      expect(user.friend?(user_2)).to be true
+      expect(user2.friend?(user)).to be true
+      expect(user.friend?(user2)).to be true
 
-      friendship = Friendship.where("user_id = ? and friend_id = ?", user.id, user_2.id).first
+      friendship = Friendship.where("user_id = ? and friend_id = ?", user.id, user2.id).first
       friendship.destroy
       expect(Friendship.count).to eql(0)
 
       user = User.first
-      user_2 = User.last
+      user2 = User.last
 
-      expect(user_2.friend?(user)).to be false
-      expect(user.friend?(user_2)).to be false
+      expect(user2.friend?(user)).to be false
+      expect(user.friend?(user2)).to be false
     end
   end
 end
