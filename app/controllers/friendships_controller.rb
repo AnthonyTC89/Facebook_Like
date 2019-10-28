f1b560d21fc640cff55902b82e21f27580d6c22f
AnthonyTC89 <ptonyptc@gmail.com>
2019-10-28 14:29:10 -0500

    validate user cannot add self

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -4,7 +4,8 @@ class FriendshipsController < ApplicationController
   def create
     @user = User.find(params[:user_id])
     @friend = User.find(params[:friend_id])
-    if @user.friend?(@friend) || @friend.friend_requests.include?(@user)
+    if @user.friend?(@friend) || @friend.friend_requests.include?(@user) 
+          || current_user.id == @friend.id
       flash[:danger] = 'Friend request not sent'
     else
       Friendship.create(user: @user, friend: @friend)
--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -4,4 +4,11 @@ class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: 'User'
   validates_uniqueness_of :user_id, scope: :friend_id
+  validate :cannot_add_self
+
+  private
+
+  def cannot_add_self
+    errors.add(:user_id, 'You cannot add yourself as a friend.') if user_id == friend_id
+  end
 end
